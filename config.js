5824ddb599293d03347f87019b5b01dd05da178b
gaoshuang <gao.gaoshuang@gmail.com>
2019-10-31 17:59:17 +0800

    init

---
+++ config.js
@@ -0,0 +1,12 @@
+const method   = { POST:'POST'};
+const headers  = {'content-type': 'application/json'}
+
+const endpoint = "http://192.168.1.58:9999";
+const version  = "/v1/wallet/";
+const keosdUrl = endpoint + version;
+
+module.exports = {
+  keosdUrl,
+  method,
+  headers
+}
